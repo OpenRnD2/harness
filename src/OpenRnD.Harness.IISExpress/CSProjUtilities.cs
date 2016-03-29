a26de6f9ff01615e243222a5b7b689abef505a43
Mathieu Renda <mathieu.renda@gmail.com>
2016-03-29 17:58:39 +0200

    Utility class to retrieve IIS URL from .csproj.

---
+++ src/OpenRnD.Harness.IISExpress/CSProjUtilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OpenRnD.Harness.IISExpress
+{
+    public static class CSProjUtilities
+    {
+        private const string msbuildNamespace = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        public static string GetIISUrl(string csProjPath)
+        {
+            if (!File.Exists(csProjPath))
+            {
+                throw new IOException($"Project file not found ({csProjPath}).");
+            }
+
+            XDocument projectDocument = XDocument.Load(csProjPath);
+            XElement webProjectProperties = projectDocument
+                .Root
+                .Element(XName.Get("ProjectExtensions", msbuildNamespace))
+                .Element(XName.Get("VisualStudio", msbuildNamespace))
+                .Elements(XName.Get("FlavorProperties", msbuildNamespace))
+                .Single(e => e.Attribute(XName.Get("GUID")).Value == "{349c5851-65df-11da-9384-00065b846f21}")
+                .Element(XName.Get("WebProjectProperties", msbuildNamespace));
+
+            string iisUrl = webProjectProperties.Element(XName.Get("IISUrl", msbuildNamespace)).Value;
+
+            return iisUrl;
+        }
+    }
+}
---
+++ test/OpenRnD.Harness.IISExpress.Tests/CSProjUtilitiesTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenRnD.Harness.IISExpress.Tests
+{
+    [TestClass]
+    public class CSProjUtilitiesTests
+    {
+        [TestMethod]
+        public void CSProjUtilities_CanRetrieveIISUrl()
+        {
+            string iisUrl = CSProjUtilities.GetIISUrl("../../../OpenRnD.Harness.IISExpress.Tests.Target/OpenRnD.Harness.IISExpress.Tests.Target.csproj");
+
+            Assert.AreEqual("http://localhost:21956/", iisUrl);
+        }
+    }
+}
